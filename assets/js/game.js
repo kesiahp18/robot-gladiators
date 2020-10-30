e0bea91d87d796f87a0986d78a729fe5fcdee83c
Kesiah Patil <kesiahpatil@Kesiahs-MacBook-Pro.local>
2020-10-30 14:52:26 -0400

    player can no longer enter blank or null name

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,19 @@ var randomNumber = function(min, max) {
 
     return value;
 }
+//FUNCTION TO SET NAME
+var getPlayerName = function() {
+    var name = "";
+    while (name == "" || name == null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robots name is "  + name);
+    return name;
+};
+
+
 var playerInfo = {
-name: window.prompt("What is your robot's name?"),
+name: getPlayerName(),
 health: 100,
 attack: 10,
 money: 10,
@@ -119,6 +130,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            debugger;
         
         //pick enemy to fight
         var pickedEnemyObj = enemyInfo[i];
